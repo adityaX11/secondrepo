a8674830fabc1a34cf159ac18e64cd9f786f154e
Aditya kumar <adicpr1234@gmail.com>
2024-10-24 03:56:10 +0530

    add kumar

--- basic.cpp
+++ basic.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
     cout<<"here the demo of github"<<endl;
-    cout<<"hii every one this is my first github expriences"<<endl;
+    cout<<"hii every one this is my first github expriences(kumar)"<<endl;
     return 0;
 }
